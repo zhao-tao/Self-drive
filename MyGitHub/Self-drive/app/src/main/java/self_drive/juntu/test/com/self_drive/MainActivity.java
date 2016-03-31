6f4020d9947c5e0bfd47b6f6579003960164710e
zhaotao <zhao-tao@msn.com>
2016-03-31 11:24:26 +0800

    首次提交测试

---
+++ MyGitHub/Self-drive/app/src/main/java/self_drive/juntu/test/com/self_drive/MainActivity.java
@@ -0,0 +1,13 @@
+package self_drive.juntu.test.com.self_drive;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
